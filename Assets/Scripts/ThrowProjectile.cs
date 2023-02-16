b1e93f7b0b39822a4b2f5810611bc413d2e9a703
Zibing Zhang <zibing.zha@gmail.com>
2023-02-16 13:29:26 -0500

    Basic projectile mechanics

---
+++ Assets/Scripts/ThrowProjectile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowProjectile : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float projectileSpeed;
+    private Transform projectileParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectileParent = GameObject.FindGameObjectWithTag("ProjectileParent").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject projectile = Instantiate(projectilePrefab,
+                transform.position + transform.forward, transform.rotation);
+            Rigidbody rigidBody = projectile.GetComponent<Rigidbody>();
+            rigidBody.AddForce(transform.forward * projectileSpeed, ForceMode.VelocityChange);
+            projectile.transform.SetParent(projectileParent);
+        }
+    }
+}
