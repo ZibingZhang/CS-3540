9fc890a9b8447d1e9c2745e465a62d221aeaccbf
Rebecca Lin <Rebecca@Rebeccas-Macbook-Pro.local>
2023-03-16 23:08:28 -0400

    destroys projectile when in contact with rock

---
+++ Assets/Scripts/SetTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject otherObject = other.gameObject;
+        if (otherObject.tag.Contains("Projectile"))
+        {
+            Debug.Log(name + " collided with projectile");
+            Destroy(otherObject);
+        }
+    }
+}
