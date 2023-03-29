a0cda1e5245b25e81be1232db57077e2a25d7d61
Zibing Zhang <zibing.zha@gmail.com>
2023-03-29 15:44:09 -0400

    animation kinda fixed, anuerysm inducing

--- Assets/NinjaAI.cs
+++ Assets/NinjaAI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NinjaAI : MonoBehaviour
 {
@@ -28,6 +29,7 @@ public class NinjaAI : MonoBehaviour
     private Transform stageBounds;
     private Vector3 enemyBounds;
 
+    //private NavMeshAgent agent;
     Vector3 target;
 
 
@@ -35,6 +37,8 @@ public class NinjaAI : MonoBehaviour
     void Start()
     {
         currentState = FSMStates.Run;
+        anim = GetComponent<Animator>();
+        anim.SetInteger("animState", 1);
 
         player = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
         projectileParent = GameObject.FindGameObjectWithTag("ProjectileParent").transform;
@@ -44,14 +48,21 @@ public class NinjaAI : MonoBehaviour
 
 
         anim = GetComponent<Animator>();
-        getRandomDestination();
+        //agent = GetComponent<NavMeshAgent>();
 
+        getRandomDestination();
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        eulerAngles.x = 0;
+        eulerAngles.z = 0;
+        transform.rotation = Quaternion.Euler(eulerAngles);
+        transform.position = new Vector3(transform.position.x, -1.5f, transform.position.z);
+
         switch (currentState)
         {
             case FSMStates.Run:
@@ -78,46 +89,64 @@ public class NinjaAI : MonoBehaviour
     }
     void UpdateRunState()
     {
-        if (Vector3.Distance(transform.position, target) < 1)
+        print(target);
+        //print(target);
+        //print(Vector3.Distance(transform.position, target));
+        if (Vector3.Distance(transform.position, target) < 3)
         {
+            hasShot = false;
             currentState = FSMStates.Punch;
+            anim.SetInteger("animState", 0);
         }
         else
         {
            // CHANGE STATE?
         }
         FaceTarget(target);
-        //anim.SetInteger("animState", 1);
-
-        transform.position = Vector3.MoveTowards(transform.position, target, enemySpeed * Time.deltaTime);
+        //agent.SetDestination(target);
+        //agent.Move(target);
+        MoveTowardsTarget(target);
+        //transform.position = Vector3.MoveTowards(transform.position, target, enemySpeed * Time.deltaTime);
     }
     void UpdateDiveState()
     {
 
     }
+
+    bool hasShot = false;
+
     void UpdatePunchState()
     {
-        //anim.SetInteger("animState", 0);
-
         FaceTarget(player.position);
 
-        if (shootDelay > 2)
+        if (shootDelay > 2 && !hasShot)
         {
-            //anim.SetInteger("animState", 100);
+            hasShot = true;
+            anim.SetInteger("animState", 101);
             //Invoke("FireAtPlayer", 10);
-            FireAtPlayer();
+            //FireAtPlayer();
 
 
             getRandomDestination();
-            currentState = FSMStates.Run;
-
+            Invoke("GoToRunState", 2);
+            Invoke("FireAtPlayer", 1);
+            
         }
         shootDelay += Time.deltaTime;
     }
+
+
+    void GoToRunState()
+    {
+        currentState = FSMStates.Run;
+        anim.SetInteger("animState", 1);
+    }
+
     void UpdateKickState()
     {
 
     }
+
     void UpdateDeadState()
     {
 
@@ -132,15 +161,25 @@ public class NinjaAI : MonoBehaviour
                                           0,
                                           Random.Range(-range.z, range.z));
         target = origin + randomPosition;
-
+        target.y = transform.position.y;
+        //target.y = gameObject.transform.position.y;
+        //print(target);
+        //agent.SetDestination(target);
     }
+
     void FaceTarget(Vector3 target)
     {
-        Vector3 directonTarget = (target - transform.position).normalized;
-        directonTarget.y = 0;
-        Quaternion lookRotation = Quaternion.LookRotation(directonTarget);
+        Vector3 directionToTarget = (target - transform.position).normalized;
+        directionToTarget.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10 * Time.deltaTime);
     }
+
+    void MoveTowardsTarget(Vector3 target)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, enemySpeed * Time.deltaTime);
+    }
+
     private void FireAtPlayer()
     {
         Vector3 direction = (player.transform.position - transform.position).normalized + new Vector3(0, 0.5f, 0);
